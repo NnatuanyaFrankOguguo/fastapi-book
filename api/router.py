1bb91d16573d9b77b5f89807c62775864c2603c4
NnatuanyaFrankOguguo <nnatuanyafrank@gmail.com>
2025-02-12 15:40:37 +0100

    ok

--- api/router.py
+++ api/router.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
 
 from api.routes import books
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI is working!"}
